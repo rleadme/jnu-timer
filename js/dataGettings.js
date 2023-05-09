c4428a73aa25071070147b41b6267f2ea29c168b
J2DCB <cjhbest1209@gmail.com>
2023-05-09 23:36:33 +0900

    작동하지 않음. 크롤링 오류(syntax)

--- js/dataGettings.js
+++ js/dataGettings.js
@@ -1,41 +1,25 @@
-const axios = require("axios");
-const iconv = require("iconv-lite");
-const cherrio = require("cheerio");
+import request from 'request';
+import { load } from 'cheerio';
+const url = "https://www.jejunu.ac.kr/ara/recruit";
 
-axios({
-  // 크롤링을 원하는 페이지 URL
-  url: "https://www.jejunu.ac.kr/ara/recruit",
-  method: "GET",
-})
-  // 성공했을 경우
-  .then((response) => {
-    const contents = iconv.decode(response.data, "EUC-KR");
-    const $ = cherrio.load(contents);
-    const titles = $(
-      "#board > table > tbody > tr:nth-child(2) > td.border_bottom.subject > a"
-    ).text();
-    console.log(titles);
-  })
-  // 실패했을 경우
-  .catch((error) => {
-    console.error(error);
-  });
-
-/*
-
-    const getHtml = async() => {
-        try{
-            return await axios.get("");
-    
-        }catch (err) {
-            console.error(err);
-        }
-    }
-    
-    getHtml()
-      .then((html) => {
-    
-        const $ = cherrio.load(html.data);
-        const data = $('#board > table > tbody > tr:nth-child(2) > td.border_bottom.subject > a');
-        return data;
-      }).then((data) => log(data));*/
+request(url, (error, response, body) => {
+  if (error) throw error;
+  let $ = load(body);
+  try{
+      var sum=0;
+      var size=0;
+      $('#board > table > tbody > tr:nth-child(2) > td.border_bottom.subject > a').each(function(index,elem){
+        var rate_text = $(this).text(); //해당 태그의 text부분만 잘라오기
+        var filtered_rate_text = rate_text.substring(0,rate_text.length-1); //'%'자르기
+        var rate = parseFloat(filtered_rate_text); //문자열 형태의 정답률을 float형으로 변환
+        console.log(rate);
+        sum+=rate;
+        size += $(this).length;
+      });
+      console.log('Size=' +size);
+      console.log('Average Correct Percentage=' +(sum/size).toFixed(3)+'%');
+  }
+  catch(error){
+      console.error(error);
+  }
+});
